cc9f7eba70cecbc151a7232dc06da84e8fdd43b4
Wellington Gallo <wellinton.gallo77@gmail.com>
2021-08-27 14:31:43 -0300

    feat: sidebar drawer

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,20 @@
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
-import React from "react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex
       align="center"
     >
-      <Box mr="4" textAlign="right">
-        <Text>Gallo</Text>
-        <Text color="gray.300" fontSize="small">wellinton.gallo77@gmail.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Gallo</Text>
+          <Text color="gray.300" fontSize="small">wellinton.gallo77@gmail.com</Text>
+        </Box>
+      )}
       <Avatar size="md" name="Wellington Gallo" />
     </Flex>
   )
---
+++ src/contexts/SidebarDrawerContext.tsx
@@ -0,0 +1,31 @@
+import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
+import { useRouter } from "next/dist/client/router";
+import { useEffect } from "react";
+import { useContext } from "react";
+import { ReactNode } from "react";
+import { createContext } from "react";
+
+interface SideBarDrawerProviderProps {
+  children: ReactNode;
+}
+
+type SidebarDrawerContextData = UseDisclosureReturn;
+
+const SidebarDrawerContext = createContext({} as SidebarDrawerContextData);
+
+export function SidebarDrawerProvider({ children }: SideBarDrawerProviderProps) {
+  const disclosure = useDisclosure();
+  const router = useRouter();
+
+  useEffect(() => {
+    disclosure.onClose();
+  }, [router.asPath]);
+
+  return (
+    <SidebarDrawerContext.Provider value={disclosure}>
+      {children}
+    </SidebarDrawerContext.Provider>
+  )
+}
+
+export const useSidebarDrawer = () => useContext(SidebarDrawerContext);
